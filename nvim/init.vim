0d8ddd18bc803098f50258504290f96426060837
mmills6060 <mmills6060@gmail.com>
2023-10-20 16:22:27 -0400

    new config files

---
+++ nvim/init.vim
@@ -0,0 +1,5 @@
+call plug#begin('~/.local/share/nvim/site/plugged')
+Plug 'junegunn/goyo.vim'
+Plug 'anott03/nvim-lspinstall'
+Plug 'ms-jpq/coq_nvim'
+call plug#end()
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-# killall -q polybar
+killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
+while pgrep -x polybar >/dev/null; do sleep 1; done
+ 
 # Launch bar1 and bar2
-#polybar base &
+polybar -r newbar
 
-sleep .5
 
-if ! pgrep -x polybar; then
-	polybar base &
-else
-	pkill -USR1 polybar
-fi
 
-echo "Bars launched..."
+
+
+
 
