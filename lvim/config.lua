382fded2b10e9a37d1ca9da840cb486990cb2708
Michael Mills <67592790+mmills6060@users.noreply.github.com>
2024-03-02 13:10:18 -0500

    Update config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -2,20 +2,26 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
-
--- -- Colorscheme
+--
+lvim.plugins = {
+{ "nordtheme/vim" },
+{ "akinsho/toggleterm.nvim"},
+{"CRAG666/code_runner.nvim", config = true},
+{"majutsushi/tagbar"},
+{"github/copilot.vim"},
+}
 lvim.colorscheme = "nord"
 
-lvim.plugins = {
-    {"nordtheme/vim"},
-    { "CRAG666/code_runner.nvim", config = true },
-
-}
-vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+--vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+--vim.keymap.set('n', '<space>h', ':ChatGPT<CR>', { noremap = false, silent = false })
+vim.keymap.set('n', '<space>t', ':TagbarToggle<CR>', { noremap = false, silent = false })
+vim.keymap.set('n', '<space>r', ':RunFile toggleterm<CR>', { noremap = false, silent = false })
 vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
+
+
 
